3080ca628c4dbb2cc50764b20fd58df458f99d34
zxinyun <holley110@hotmail.com>
2026-04-03 14:02:19 +0800

    Update Editor.tsx

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -68,7 +68,7 @@ export default function Editor() {
               <Md2PosterContent>{mdString}</Md2PosterContent>
               <Md2PosterFooter className='text-center'>
                 <img src="/logo.png" alt="logo" className='inline-block mr-2 w-5' />
-                Powered by markdown-to-image-serve.jcommon.top
+                Powered by 洋芋哥
               </Md2PosterFooter>
             </Md2Poster>
           </div>
